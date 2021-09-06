add60fe47ca797b4b55778ac1af98b761c79bd3c
Jie Wei Kong <32333304+Joash-JW@users.noreply.github.com>
2021-09-07 00:23:57 +0800

    added db URL for heroku deploy

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ from random import choice
 import string, logging
 
 app = Flask(__name__, template_folder='./frontend', static_folder='./frontend')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tuzhmmegshfmyv:c4a1cfa40a3b3eb182adaa3616137bceeb613fe5331b575fa861d2d6d4624a9f@ec2-44-196-8-220.compute-1.amazonaws.com:5432/d4n89ir995sblm'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
